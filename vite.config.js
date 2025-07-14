af7c420b38f25df9c80e91b6cd5013d8d1833eae
Zekeyy <ezekielpilar27@gmail.com>
2025-07-13 21:28:01 -0700

    new path6

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:"/pilar-portfolio" // Note the trailing slash
+  base: process.env.VITE_BASE_PATH || "/pilar-portfolio" // Note the trailing slash
 
 })
