a2f6be9cb69759a0587cbebfb5c15a1a6fc5d906
Zekeyy <146742405+Zekeyy@users.noreply.github.com>
2025-07-24 04:29:20 -0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -34,7 +34,7 @@ const Footer = ({ isDarkMode }) => {
               } hover:bg-blue-700 hover:text-white hover:scale-125 transition-all duration-300 text-xl`}
             >
               {social.icon.endsWith('.png') || social.icon.endsWith('.jpg') || social.icon.endsWith('.svg') ? (
-                <img src={social.icon} alt="icon" className="w-12 h-10" />
+                <img src={social.icon} alt="icon" className="w-8 h-10" />
               ) : (
                 social.icon
               )}
